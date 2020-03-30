d3e8b410197eeadf37a297a9a88d1b9f51273f31
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-29 20:17:10 -0400

    saving for now

---
+++ src/java/main/DashaMapsOne.java
@@ -0,0 +1,47 @@
+package main;
+
+public class DashaMapsOne implements HashMapX{
+   private Node[] dashaOneNodes = new Node[26];
+   private Integer size = 0;
+
+    public DashaMapsOne() {
+
+    }
+
+    private String HashFunctionOne(String input) {
+        if (input.length() > 0) {
+            return String.valueOf(input.charAt(0)).toLowerCase();
+        }
+        return null;
+    }
+
+    @Override
+    public void set(String key, String value) {
+
+    }
+
+    @Override
+    public String delete(String key) {
+        return null;
+    }
+
+    @Override
+    public String get(String key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public long size() {
+        return 0;
+    }
+
+    @Override
+    public boolean bucketSize(String key) {
+        return false;
+    }
+}
